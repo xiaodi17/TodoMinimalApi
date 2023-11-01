97479b403a6b55fcfda373bc8e5ea6748fda2774
David <xiaodi.david@gmail.com>
2023-11-01 19:30:53 +1000

    Project Setup

---
+++ Todo.cs
@@ -0,0 +1,7 @@
+namespace TodoMinimalApi;
+public class Todo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
---
+++ TodoDb.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoMinimalApi;
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
